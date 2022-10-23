e425ac14bce24bf9dd8bf3408a0a8f62df5f8ad2
Chayank11 <chayank.tm@gmail.com>
2022-10-23 19:26:08 -0400

    message jsdoc completed

--- controllers/messages/MessageController.ts
+++ controllers/messages/MessageController.ts
@@ -1,37 +1,80 @@
+/**
+ * @file Controller for handling messages
+ */
 import {Request, Response, Express} from "express";
 import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import MessageDao from "../../daos/messages/MessageDao";
 import MessageControllerI from "../../interfaces/messages/MessageController";
 
+/**
+ * @class This class implements the RESTful web service api for handling follows related operations.
+ * @property {MessageDao} messageDao implementing the CRUD operations
+ * @property {MessageController} messageController implementing the CRUD APIs
+ */
 export default class MessageController implements MessageControllerI {
-   app: Express;
-   messageDao: MessageDao;
+    private static messageDao: MessageDao = MessageDao.getInstance();
+    private static messageController: MessageController | null = null;
 
-   constructor(app: Express, messageDao: MessageDao) {
-       this.app = app;
-       this.messageDao = messageDao;
-       
-       this.app.post('/users/:uid/messages/:uid2', this.userMessageAnotherUser);
-       this.app.delete('/users/:uid/messages/:uid2', this.userDeletesMessageWithAnotherUser);
-       this.app.get('/users/:uid/sent-messages', this.findAllMessagesSent);
-       this.app.get('/users/:uid/rcvd-messages', this.findAllMessagesRcvd);
-   }
-   
+    /**
+     * Creates a singleton controller instance
+     * @param {Express} app An express instance to declare the Web service api
+     * @returns messageController
+     */
+    public static getInstance = (app: Express): MessageController => {
+        if (MessageController.messageController === null) {
+            MessageController.messageController = new MessageController();
+            app.post('/users/:uid/messages/:uid2', MessageController.messageController.userMessageAnotherUser);
+            app.delete('/users/:uid/messages/:uid2', MessageController.messageController.userDeletesMessageWithAnotherUser);
+            app.get('/users/:uid/sent-messages', MessageController.messageController.findAllMessagesSent);
+            app.get('/users/:uid/rcvd-messages', MessageController.messageController.findAllMessagesRcvd);
+        }
+        return MessageController.messageController;
+    }
+
+    private constructor() {
+    }
+
+    /**
+     * Inserts a message instance into messages collection in the database
+     * @param {Request} req is the request from clients with
+     * uid1 as primary key of user whos sends the message, uid2 as the primary key
+     * of the user to whom the message is sent and message to be sent
+     * @param {Response} res is the response to the client as JSON that contains
+     * the message instance that was added in the database
+     */
     userMessageAnotherUser = (req: Request, res: Response) =>
-        this.messageDao.userMessageAnotherUser(req.body)
+        MessageController.messageDao.userMessageAnotherUser(req.body)
         .then(msg => res.json(msg));
 
+    /**
+     * Deletes a message instance from the messages collection in the database
+     * @param {Request} req is the request from clients with
+     * message object in request body
+     * @param {Response} res is the response to the client that contains the delete status
+     */
     userDeletesMessageWithAnotherUser = (req: Request, res: Response) =>
-        this.messageDao.userDeletesMessageWithAnotherUser(req.body)
+        MessageController.messageDao.userDeletesMessageWithAnotherUser(req.body)
         .then(status => res.json(status));
 
+    /**
+     * Retrieves all the messages sent by a  user
+     * @param {Request} req is the request from clients with uid as the primary
+     * key of the user who has sent messages
+     * @param {Response} res is the response to the client as JSON that contains messages list
+     */
     findAllMessagesSent = (req: Request, res: Response) =>
-        this.messageDao.findAllMessagesSent(req.params.uid)
+        MessageController.messageDao.findAllMessagesSent(req.params.uid)
         .then(msgs => res.json(msgs));
 
+    /**
+     * Retrieves all the messages received by a  user
+     * @param {Request} req is the request from clients with uid as the primary
+     * key of the user who has received messages
+     * @param {Response} res is the response to the client as JSON that contains messages list
+     */
     findAllMessagesRcvd = (req: Request, res: Response) =>
-        this.messageDao.findAllMessagesRcvd(req.params.uid)
+        MessageController.messageDao.findAllMessagesRcvd(req.params.uid)
         .then(msgs => res.json(msgs));
 
 }
--- daos/messages/MessageDao.ts
+++ daos/messages/MessageDao.ts
@@ -1,25 +1,67 @@
-
+/**
+ * @file Implements management and  data storage of messages.
+ */
 import MessageDaoI from "../../interfaces/messages/MessageDao";
 import { json } from "body-parser";
 import Message from "../../models/messages/Message";
 import MessageModel from "../../mongoose/messages/MessageModel";
 
-
+/**
+ * @class MessageDao Implements Data Access Object managing data storage of messages
+ * @implements {MessageDaoI} MessageDaoI
+ * @property {MessageDao} messageDao Private single instance of MessageDao
+ */
 export default class MessageDao implements MessageDaoI {
 
+    private static messageDao:MessageDao | null = null;
+
+    /**
+     * Creates a singleton DAO instance
+     * @returns MessageDao
+     */
+    public static getInstance = (): MessageDao => {
+        if(MessageDao.messageDao === null){
+            MessageDao.messageDao = new MessageDao();
+        }
+        return MessageDao.messageDao;
+    }
+
+    private constructor() {
+    }
+
+    /**
+     * Creates a message instance into messages collection in the database
+     * @param {Message} msg the content(instance) to be inserted into the database
+     * @returns {Promise} To be notified when the message instance is created in the database
+     */
     async userMessageAnotherUser(msg : Message): Promise<Message> {
         const new_message : any = await MessageModel.create(msg)
         return new_message;
     }
 
+    /**
+     * Deletes a message instance from messages collection in the database
+     * @param {Message} msg message JSON object for delete
+     * @returns {Promise} To be notified when the message instance is removed from the database
+     */
     async userDeletesMessageWithAnotherUser(msg : any): Promise<any> {
         return await MessageModel.deleteOne({_id: msg._id});
     }
 
+    /**
+     * Retrieves all the messages sent by a user
+     * @param {string} uid User's primary key
+     * @returns {Promise} To be notified when the messages are retrieved from database
+     */
     async findAllMessagesSent(uid: string): Promise<Message[]> {
         return await MessageModel.find({from : uid})
     }
 
+    /**
+     * Retrieves all the messages received by a user
+     * @param {string} uid User's primary key
+     * @returns {Promise} To be notified when the messages are retrieved from database
+     */
     async findAllMessagesRcvd(uid: string): Promise<Message[]> {
         return await MessageModel.find({to : uid})
     }
