2befd1371d12b826f09b8245c5631aeb00034436
Chayank11 <chayank.tm@gmail.com>
2022-10-23 19:48:42 -0400

    bookmark jsdoc created

--- controllers/bookmarks/BookmarkController.ts
+++ controllers/bookmarks/BookmarkController.ts
@@ -1,3 +1,6 @@
+/**
+ * @file Controller for handling bookmarks
+ */
 import {Request, Response, Express} from "express";
 import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
@@ -5,29 +8,62 @@ import BookmarkDao from "../../daos/bookmarks/BookmarkDao";
 import BookmarkControllerI from "../../interfaces/bookmarks/BookmarkController";
 import Bookmark from "../../models/bookmarks/Bookmark";
 
+/**
+ * @class This class implements the RESTful web service api for handling bookmarks related operations.
+ * @property {BookmarkDao} bookmarkDao implementing the CRUD operations
+ * @property {BookmarkController} bookmarkController implementing the CRUD APIs
+ */
 export default class BookmarkController implements BookmarkControllerI {
-   app: Express;
-   bookmarkDao: BookmarkDao;
+    private static bookmarkDao:BookmarkDao = BookmarkDao.getInstance();
+    private static bookmarkController:BookmarkController | null = null;
 
-   constructor(app: Express, bookmarkDao: BookmarkDao) {
-       this.app = app;
-       this.bookmarkDao = bookmarkDao;
-       
-       this.app.post('/users/:uid/bookmarks/:tid', this.userBookmarksTuit);
-       this.app.delete('/users/:uid/bookmarks/:tid', this.userUnBookmarksTuit);
-       this.app.get('/users/:uid/bookmarks', this.findAllTuitsBookmarkedByUser);
-   }
+    /**
+     * Creates a singleton controller instance
+     * @param {Express} app An express instance to declare the Web service api
+     * @returns bookmarkController
+     */
+    public static getInstance = (app:Express): BookmarkController => {
+        if(BookmarkController.bookmarkController === null){
+            BookmarkController.bookmarkController = new BookmarkController();
+            app.get("/users/:uid/bookmarks", BookmarkController.bookmarkController.findAllTuitsBookmarkedByUser);
+            app.post("/users/:uid/bookmarks/:tid", BookmarkController.bookmarkController.userBookmarksTuit);
+            app.delete("/users/:uid/bookmarks/:tid", BookmarkController.bookmarkController.userUnBookmarksTuit);
+        }
+        return BookmarkController.bookmarkController;
+    }
 
+    private constructor() {
+    }
+
+    /**
+     * Inserts a bookmark instance into bookmarks collection in the database
+     * @param {Request} req is the request from client, with uid as the primary key of the user who
+     * bookmarks a tuit and tid being the primary key of the tuit that is bookmarked
+     * @param {Response} res Represents response to client as JSON that contains the bookmark instance that is added in
+     * the database
+     */
     userBookmarksTuit = (req: Request, res: Response) =>
-        this.bookmarkDao.userBookmarksTuit(req.params.uid, req.params.tid)
+    BookmarkController.bookmarkDao.userBookmarksTuit(req.params.uid, req.params.tid)
         .then(bookmark => res.json(bookmark));
 
+
+    /**
+     * Deletes a bookmark instance from bookmarks collection in the database
+     * @param {Request} req is the request from client, with uid as the primary key of the user who
+     * bookmarks a tuit and tid being the primary key of the tuit that is bookmarked
+     * @param {Response} res is the response to the client that contains the delete status
+     */
     userUnBookmarksTuit = (req: Request, res: Response) =>
-        this.bookmarkDao.userUnBookmarksTuit(req.params.uid, req.params.tid)
+    BookmarkController.bookmarkDao.userUnBookmarksTuit(req.params.uid, req.params.tid)
         .then(status => res.json(status));
 
+    /**
+     * Retrieves all the tuits bookmarked by a particular user
+     * @param {Request} req is the request from clients
+     * @param {Response} res is the response to the client as JSON containing all tuits that are bookmarked
+     */   
     findAllTuitsBookmarkedByUser = (req: Request, res: Response) =>
-        this.bookmarkDao.findAllTuitsBookmarkedByUser(req.params.uid)
+    BookmarkController.bookmarkDao.findAllTuitsBookmarkedByUser(req.params.uid)
         .then(tuits => res.json(tuits));
 
 }
--- daos/bookmarks/BookmarkDao.ts
+++ daos/bookmarks/BookmarkDao.ts
@@ -1,3 +1,6 @@
+/**
+ * @file Implements management and  data storage of bookmarks.
+ */
 import { json } from "body-parser";
 import BookmarkDaoI from "../../interfaces/bookmarks/BookmarkDao";
 import Bookmark from "../../models/bookmarks/Bookmark";
@@ -5,8 +8,35 @@ import Tuit from "../../models/tuits/Tuit";
 import BookmarkModel from "../../mongoose/bookmarks/BookmarkModel";
 import TuitModel from "../../mongoose/tuits/TuitModel";
 
+/**
+ * @class BookmarkDao Implements Data Access Object managing data storage of bookmarks
+ * @implements {BookmarkDaoI} BookmarkDaoI
+ * @property {BookmarkDao} bookmarkDao Private single instance of BookmarkDao
+ */
 export default class BookmarkDao implements BookmarkDaoI {
 
+    private static bookmarkDao: BookmarkDao | null = null;
+
+    /**
+     * Creates a singleton DAO instance
+     * @returns BookmarkDao
+     */
+    public static getInstance = (): BookmarkDao => {
+        if(BookmarkDao.bookmarkDao === null){
+            BookmarkDao.bookmarkDao = new BookmarkDao();
+        }
+        return BookmarkDao.bookmarkDao;
+    }
+
+    private constructor() {
+    }
+
+    /**
+     * Inserts a bookmark instance into bookmarks collection in the database
+     * @param {string} uid primary key of user who bookmarked a tuit
+     * @param {string} tid primary key of the tuit that is bookmarked by the user
+     * @returns {Promise} To be notified when the bookmark instance is created in the database
+     */
     async userBookmarksTuit(uid: string, tid: string): Promise<Bookmark> {
 
         const exist_bookmark : Bookmark = await BookmarkModel
@@ -21,6 +51,12 @@ export default class BookmarkDao implements BookmarkDaoI {
         return exist_bookmark;
     }
 
+    /**
+     * Deletes a bookmark instance from bookmarks collection in the database
+     * @param {string} uid primary key of user who wants to unbookmark a tuit
+     * @param {string} tid primary key of the tuit that is unbookmarked by the user
+     * @returns {Promise} To be notified when the bookmark instance is removed from the database
+     */
     async userUnBookmarksTuit(uid: string, tid: string): Promise<any> {
         const exist_bookmark : any = await BookmarkModel
         .findOne({bookmarkedTuit : tid, bookmarkedBy : uid})
@@ -31,6 +67,11 @@ export default class BookmarkDao implements BookmarkDaoI {
         return false;
     }
 
+    /**
+     * Retrieves all the tuits bookmarked by a  user
+     * @param {string} uid User's primary key
+     * @returns {Promise} To be notified when the bookmarked tuits are retrieved from database
+     */
     async findAllTuitsBookmarkedByUser(uid: string): Promise<Tuit[]> {
         const bookmarks : any =  await BookmarkModel
         .find({bookmarkedBy : uid})
