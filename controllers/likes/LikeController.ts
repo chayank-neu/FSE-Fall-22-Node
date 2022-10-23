2dd2f992faa117d822e9bba832887f3f10ce509e
Chayank11 <chayank.tm@gmail.com>
2022-10-23 19:35:28 -0400

    like jsdoc completed

--- controllers/likes/LikeController.ts
+++ controllers/likes/LikeController.ts
@@ -1,36 +1,79 @@
+/**
+ * @file Controller for handling likes
+ */
+
 import {Request, Response, Express} from "express";
 import { ParamsDictionary } from "express-serve-static-core";
 import { ParsedQs } from "qs";
 import LikeDao from "../../daos/likes/LikeDao";
 import LikeControllerI from "../../interfaces/likes/LikeController";
 
+/**
+ * @class This class implements the RESTful web service api for handling likes related operations.
+ * @property {LikeDao} likeDao implementing the CRUD operations
+ * @property {LikeController} likeController implementing the CRUD APIs
+ */
 export default class LikeController implements LikeControllerI {
-   app: Express;
-   likeDao: LikeDao;
+    private static likeDao:LikeDao = LikeDao.getInstance();
+    private static likeController: LikeController | null = null;
 
-   constructor(app: Express, likeDao: LikeDao) {
-       this.app = app;
-       this.likeDao = likeDao;
-       
-       this.app.post('/users/:uid/likes/:tid', this.addLikeToTuit);
-       this.app.delete('/users/:uid/likes/:tid', this.removeLikeToTuit);
-       this.app.get('/users/:uid/likes', this.findAllTuitsLikedByUser);
-       this.app.get('/tuits/:tid/likes', this.findAllUsersByTuitLike);
-   }
+    /**
+     * Creates a singleton controller instance
+     * @param {Express} app An express instance to declare the Web service api
+     * @returns likeController
+     */
+    public static getInstance = (app: Express): LikeController => {
+        if(LikeController.likeController === null){
+            LikeController.likeController = new LikeController();
+
+            app.post('/users/:uid/likes/:tid', LikeController.likeController.addLikeToTuit);
+            app.delete('/users/:uid/likes/:tid', LikeController.likeController.removeLikeToTuit);
+            app.get('/users/:uid/likes', LikeController.likeController.findAllTuitsLikedByUser);
+            app.get('/tuits/:tid/likes', LikeController.likeController.findAllUsersByTuitLike);
+
+        }
+        return LikeController.likeController;
+    }
+    private constructor() {
+    }
+
+    /**
+     * Inserts a new like instance into the likes collection in the database
+     * @param {Request} req is the request from clients with tid as the tuit's primary key
+     * and uid as the user's primary key
+     * @param {Response} res is the response to the client as JSON which contains the like
+     * that was inserted in the databases
+     */
     addLikeToTuit = (req: Request, res: Response) =>
-        this.likeDao.addLikeToTuit(req.params.uid, req.params.tid)
+        LikeController.likeDao.addLikeToTuit(req.params.uid, req.params.tid)
         .then(like => res.json(like));
 
+    /**
+     * Deletes a like instance from the likes collection in the database
+     * @param {Request} req is the request from clients with tid as the tuit's primary key
+     * and uid as the user's primary key
+     * @param {Response} res is the response to the client with delete status
+     */
     removeLikeToTuit = (req: Request, res: Response) =>
-        this.likeDao.removeLikeToTuit(req.params.uid, req.params.tid)
+        LikeController.likeDao.removeLikeToTuit(req.params.uid, req.params.tid)
         .then(status => res.json(status));
 
+    /**
+     * Retrieves all the tuits liked by a particular user
+     * @param {Request} req is the request from clients with uid as the users primary key
+     * @param {Response} res is the response to the client as JSON that contains tuits list
+     */
     findAllTuitsLikedByUser = (req: Request, res: Response) =>
-        this.likeDao.findAllTuitsLikedByUser(req.params.uid)
+        LikeController.likeDao.findAllTuitsLikedByUser(req.params.uid)
         .then(tuits => res.json(tuits));
-
+    
+    /**
+     * Retrieves all the users that liked by a particular tuit
+     * @param {Request} req is the request from clients with tid as the tuit's primary key
+     * @param {Response} res is the response to the client as JSON which contains users list
+     */
     findAllUsersByTuitLike = (req: Request, res: Response) =>
-        this.likeDao.findAllUsersByTuitLike(req.params.tid)
+        LikeController.likeDao.findAllUsersByTuitLike(req.params.tid)
         .then(users => res.json(users));
 
     
--- daos/likes/LikeDao.ts
+++ daos/likes/LikeDao.ts
@@ -1,3 +1,7 @@
+/**
+ * @file Implements management and  data storage of likes.
+ */
+
 import Tuit from "../../models/tuits/Tuit";
 import TuitModel from "../../mongoose/tuits/TuitModel";
 import LikeDaoI from "../../interfaces/likes/LikeDao";
@@ -6,7 +10,36 @@ import User from "../../models/users/User";
 import { json } from "body-parser";
 import Like from "../../models/likes/Like";
 import LikeModel from "../../mongoose/likes/LikeModel";
+
+/**
+ * @class LikeDao Implements Data Access Object managing data storage of likes
+ * @implements {LikeDaoI} LikeDaoI
+ * @property {LikeDao} likeDao Private single instance of LikeDao
+ */
 export default class LikeDao implements LikeDaoI {
+
+    private static likeDao: LikeDao | null = null;
+
+    /**
+     * Creates a singleton DAO instance
+     * @returns LikeDao
+     */
+    public static getInstance = (): LikeDao => {
+        if(LikeDao.likeDao === null){
+            LikeDao.likeDao = new LikeDao();
+        }
+        return LikeDao.likeDao;
+    }
+
+    private constructor() {
+    }
+
+    /**
+     * Inserts a new like instance into the likes collection in the database
+     * @param {string} uid User's primary key
+     * @param {string} tid Tuit's primary key
+     * @returns {Promise} To be notified when the like instance is inserted into the database
+     */
     async addLikeToTuit(uid: string, tid: string): Promise<Like> {
         const exist_like : any = await LikeModel.findOne({tuit : tid, likedBy : uid})
         if (exist_like == null) {
@@ -18,6 +51,12 @@ export default class LikeDao implements LikeDaoI {
         return exist_like;
     }
 
+    /**
+     * Deletes a like instance from the likes collection in the database
+     * @param {string} uid User's primary key
+     * @param {string} tid Tuit's primary key
+     * @returns {Promise} To be notified when the like instance is deleted from the database
+     */
     async removeLikeToTuit(uid: string, tid: string): Promise<any> {
         const exist_like : any = await LikeModel.findOne({tuit : tid, likedBy : uid})
         if (exist_like != null) {
@@ -26,6 +65,11 @@ export default class LikeDao implements LikeDaoI {
         return false;
     }
 
+    /**
+     * Retrieves all the tuits liked by a particular user
+     * @param {string} uid User's primary key
+     * @returns {Promise} To be notified when the tuits are retrieved from database
+     */
     async findAllTuitsLikedByUser(uid: string): Promise<Tuit[]> {
         const likes : any =  await LikeModel.find({likedBy: uid});
 
@@ -44,6 +88,11 @@ export default class LikeDao implements LikeDaoI {
         return tuits
     }
 
+    /**
+     * Retrieves all the users that liked by a particular tuit
+     * @param {string} tid Tuit's primary key
+     * @returns {Promise} To be notified when the users are retrieved from database
+     */
     async findAllUsersByTuitLike(tid: string): Promise<User[]> {
         const likes : any =  await LikeModel.find({tuit: tid});
         const users: User[] | PromiseLike<User[]> = [];
