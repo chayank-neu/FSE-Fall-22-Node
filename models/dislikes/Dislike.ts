3b569b6d483886c86211d52412e56adbb3c0b903
Chayank11 <chayank.tm@gmail.com>
2022-11-27 17:52:45 -0500

    dislike model class

---
+++ models/dislikes/Dislike.ts
@@ -0,0 +1,19 @@
+/**
+ * @file shows the model of the dislike
+ */
+
+/**
+ * @class Represents the dislike relation between user and tuit
+ * @property {Tuit} tuit the tuit disliked
+ * @property {User} likedBy user disliked the tuit
+ */
+ export default class Dislike {
+
+    private tuit: string = '';
+    private dislikedBy: string;
+
+    constructor(tuit?: string, dislikedBy?: string) {
+           this.tuit = tuit||'';
+           this.dislikedBy = dislikedBy||"";
+        }
+}
